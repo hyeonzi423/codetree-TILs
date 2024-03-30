e1669b9e24a7921b5ee88d425d197ca09a196cbd
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-03-31 01:27:33 +0900

    Title: 나무박멸, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240331/나무박멸/tree-kill-all.java
+++ 240331/나무박멸/tree-kill-all.java
@@ -8,6 +8,24 @@ public class Main {
 	static int N, M, K, C, ans;
 	static int[][] tree, tmpTree, herbicide;
 	static int dx[] = { -1, 0, 1, 0 };
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M, K, C, ans;
+	static int[][] tree, tmpTree, herbicide;
+	static int dx[] = { -1, 0, 1, 0 };
+
+코드를 실행시키는 중입니다.
+
+public
+
+정상적으로 제출되었습니다.
+
+
 	static int dy[] = { 0, 1, 0, -1 };
 	static int hdx[] = { -1, -1, 1, 1 };
 	static int hdy[] = { -1, 1, -1, 1 };
@@ -35,13 +53,17 @@ public class Main {
 		ans = 0;
 		for(int i = 0; i < M; i++) {
 			grow();
+			//System.out.println("grow");
 			//print();
 			spread();
+			//System.out.println("spread");
 			//print();
 			choice();
+			//System.out.println("choice");
 			//print1();
 			remove();
 			//print();
+			//System.out.println(ans + "!!!!!!!!!!!!!!");
 		}
 		System.out.println(ans);
 	}
@@ -58,7 +80,7 @@ public class Main {
 					tmpTree[i][j] = -100;
 					continue;
 				}
-				if (tree[i][j] <= 0) {
+				else if (tree[i][j] <= 0) {
 					tmpTree[i][j] = tree[i][j];
 					continue;
 				}
@@ -85,7 +107,7 @@ public class Main {
 		tmpTree = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (tree[i][j] == 0) {
+				if (tree[i][j] <= 0) {
 					continue;
 				}
 				else if(tree[i][j] == -100) {
@@ -125,7 +147,7 @@ public class Main {
 		int max = 0, maxX = -1, maxY = -1;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (tree[i][j] == 0 || tree[i][j] == -100)
+				if (tree[i][j] <= 0 || tree[i][j] == -100)
 					continue;
 				int cnt = tree[i][j];
 				for (int a = 0; a < 4; a++) {
@@ -149,6 +171,11 @@ public class Main {
 		}
 		if(maxX == -1 && maxY == -1) return;
 		ans += max;
+		
+//		System.out.println();
+//        System.out.println(maxX + " " + maxY);
+//        System.out.println();
+		
 		tree[maxX][maxY] = -(C+1);
 		for (int a = 0; a < 4; a++) {
 			for (int b = 1; b <= K; b++) {
@@ -156,6 +183,13 @@ public class Main {
 				int ny = maxY + hdy[a]*b;
 				if (!inRange(nx, ny))
 					break;
+				else if(tree[nx][ny] == -100) {
+					break;
+				}
+				else if(tree[nx][ny] <= 0) {
+					tree[nx][ny] = -(C + 1);
+					break;
+				}
 				else {
 					tree[nx][ny] = -(C + 1);
 				}
